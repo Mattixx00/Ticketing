3f221bbd3f1679a0f815a27567e7cb843081a12d
manuel.varani@aldini.istruzioneer.it <manuel.varani@aldini.istruzioneer.it>
2023-05-03 10:17:10 +0200

    Update login.java

--- login.java
+++ login.java
@@ -6,6 +6,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * Servlet implementation class login
@@ -34,6 +38,28 @@ public class login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+	     try {
+             // Carica il driver JDBC per il database
+             Class.forName("com.mysql.cj.jdbc.Driver");
+             PreparedStatement stmt= null;
+             // Ottieni la connessione al database
+             String url = "jdbc:mysql://localhost:3306/ticketing";
+             String user = "root";
+             String password = "";
+             Connection conn = DriverManager.getConnection(url, user, password);
+
+             // Esegui la query per inserire i dati nella tabella della registrazione
+             String sql = "SELECT * FROM utente WHERE username=? and password=?";
+             stmt = conn.prepareStatement(sql);
+             stmt.setString(1, request.getParameter("username"));
+             stmt.setString(1, request.getParameter("password"));
+             ResultSet rowsInserted = stmt.executeQuery();
+             
+             while (rs.next()) {
+            	 
+             }
+         }
 	}
 
 }
