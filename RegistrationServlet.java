a124bbb782e5f6eb3140bd8c3bec5a915359b417
Mattia <mattia.palma@aldini.istruzioneer.it>
2023-05-05 21:30:51 +0200

    cambio da javax a jakarta
    forza napoli

--- RegistrationServlet.java
+++ RegistrationServlet.java
@@ -4,10 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.json.simple.JSONObject;
 
 public class RegistrationServlet extends HttpServlet {
