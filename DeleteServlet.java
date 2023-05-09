4145f5db253126ad987f78fe9619b3a78dfa5a03
Mattia <mattia.palma@aldini.istruzioneer.it>
2023-05-09 19:43:08 +0200

    Update DeleteServlet.java

--- DeleteServlet.java
+++ DeleteServlet.java
@@ -1,46 +1,53 @@
+package com.example.login;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-public class DeleteServlet extends HttpServlet {
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "Delete", value = "/Delete")
+public class Delete extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
         // Leggi i dati dal POST
-        String username = request.getParameter("username");
+        int id = Integer.parseInt(request.getParameter("id"));
 
-        boolean deleteSuccess = deleteRecord(username);
+        boolean deleteSuccess = deleteRecord(id);
 
         // Imposta l'intestazione della risposta come JSON
         response.setContentType("application/json");
 
         //Json
-        JSONObject jsonResponse = new JSONObject();
+        JsonObject jsonResponse = new JsonObject();
 
         // Crea un oggetto JSON per la risposta
-        jsonResponse.put("status-delete", deleteSuccess ? "success" : "failed");
+        jsonResponse.addProperty("status-delete", deleteSuccess ? "success" : "failed");
         PrintWriter out = response.getWriter();
-        out.print(jsonResponse.toJSONString());
+        out.print(jsonResponse.toString());
         out.flush();
     }
 
-    private boolean deleteRecord(String username) {
+    private boolean deleteRecord(int id) {
         boolean deleteSuccess = false;
         Connection conn = null;
         PreparedStatement stmt = null;
 
         try {
             // Carica il driver JDBC per il database
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
 
             // Ottieni la connessione al database
             String url = "jdbc:mysql://localhost:3306/ticketing";
@@ -49,9 +56,9 @@ public class DeleteServlet extends HttpServlet {
             conn = DriverManager.getConnection(url, user, password);
 
             // Esegui la query per cancellare il record dalla tabella
-            String sql = "DELETE FROM utenti WHERE username=?";
+            String sql = "DELETE FROM utente WHERE ID=?";
             stmt = conn.prepareStatement(sql);
-            stmt.setInt(1, username);
+            stmt.setInt(1, id);
             int rowsDeleted = stmt.executeUpdate();
 
             if (rowsDeleted > 0) {
