2b659a856d07622be982662c439c2645016c7759
Mattia <mattia.palma@aldini.istruzioneer.it>
2023-05-10 08:55:33 +0200

    Update DeleteTicketingServlet.java

--- DeleteTicketingServlet.java
+++ DeleteTicketingServlet.java
@@ -1,22 +1,21 @@
-package it.avbo.progettoNSI;
+package com.example.login;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
+import com.google.gson.JsonObject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/ticketing")
-public class DeleteTicketingServlet extends HttpServlet {
+@WebServlet(name = "DeleteTicket", value = "/DeleteTicket")
+public class Login extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     // Impostazioni per la connessione al database
@@ -27,35 +26,49 @@ public class DeleteTicketingServlet extends HttpServlet {
     /**
      * Metodo che viene chiamato quando l'utente invia una richiesta per eliminare un ticket.
      */
- protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         // Recupera l'ID del ticket da eliminare
         int ticketId = Integer.parseInt(request.getParameter("ticketId"));
 
         // Verifica se l'utente ha cliccato sul pulsante per eliminare il ticket
-        if ("delete".equals(request.getParameter("action"))) {
-            // Elimina il ticket dal database
-            deleteTicket(ticketId);
-        } 
-        // Rimanda l'utente alla pagina per visualizzare i ticket aggiornati
-        response.sendRedirect(request.getContextPath() + "/ticketing");
+
+        boolean status=deleteTicket(ticketId);
+
+        JsonObject stato=new JsonObject();
+        if(status){
+            stato.addProperty("stato","success");
+        }else{
+            stato.addProperty("stato","failure");
+        }
+        PrintWriter out = response.getWriter();
+        out.print(stato.toString());
+        out.flush();
     }
-    
-    /**
-     * Metodo che elimina un ticket dal database dato l'ID del ticket.
-     * @param id ID del ticket da eliminare
-     */
-    private void deleteTicket(int id) {
+    private boolean deleteTicket(int id) {
+        boolean status=false;
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            String sql = "DELETE FROM tickets WHERE id=?";
+            String sql = "DELETE FROM ticket WHERE ID=?";
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setInt(1, id);
-            statement.executeUpdate();
+            int row=statement.executeUpdate();
+
+            if(row>0){
+                status=true;
+            }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+
+        return status;
     }
 
-    
+
 
 }
